e3e0afe3d1399123baa55b73061f0926d765dc41
James Xu <xumingming64398966@gmail.com>
2013-01-21 17:25:33 +0800

    Add magit, projectile

---
+++ .emacs.d/elpa/projectile-0.8/projectile-pkg.el
@@ -0,0 +1,1 @@
+(define-package "projectile" "0.8" "Manage and navigate projects in Emacs easily" (quote ((s "1.0.0") (dash "1.0.0"))))
---
+++ .emacs.d/elpa/s-1.3.0/s-pkg.el
@@ -0,0 +1,1 @@
+(define-package "s" "1.3.0" "The long lost Emacs string manipulation library." (quote nil))
