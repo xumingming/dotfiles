1da425caea2b994ba43a606f00814040ff95b2c6
James Xu <xumingming64398966@gmail.com>
2013-01-31 19:40:55 +0800

    Added dotfiles setup script

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This script is used to setup the nessisary soft links for the dotfiles
+
+# Setup the specified soft link
+setup_link() {
+    path=$1
+    from_path=~/local/svn/dotfiles/$path
+    to_path=~/$path
+    if [ ! -d $to_path ]; then
+        /bin/ln -s $from_path $to_path
+        echo "Copy $from_path to $to_path"
+    else
+        echo "WARNNING: $to_path already exists, so will not copy."
+    fi
+}
+
+setup_link ".lein/profiles.clj"
+setup_link ".emacs"
+setup_link ".emacs.d"
+
+
