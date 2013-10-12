f8f5c979617ecd228505721a0d6e3a36256436b9
James Xu <xumingming64398966@gmail.com>
2013-10-12 09:11:37 +0800

    add eastwood and lein-eclipse plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,6 @@
-{:user {:plugins [[lein-ritz "0.7.0"]]
+{:user {:plugins [[lein-ritz "0.7.0"]
+                  [no-man-is-an-island/lein-eclipse "2.0.0"]
+                  [jonase/eastwood "0.0.2"]]
         :dependencies [
                        [ritz/ritz-nrepl-middleware "0.7.0"]]
         :repl-options { ;;:init (use 'tracer.core)
