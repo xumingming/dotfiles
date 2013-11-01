5a8d2e1d67c668e008882e5db755a1888c6e516b
James Xu <xumingming64398966@gmail.com>
2013-11-01 15:15:06 +0800

    add many xxx_one functions

--- hiveservice.sh
+++ hiveservice.sh
@@ -1,62 +1,100 @@
 #!/bin/bash
 server_ips=(localhost 10.209.22.186 10.209.125.24)
 #server_ips=(10.227.16.78 10.227.16.79 10.227.16.80 10.227.14.22 10.227.10.164 10.227.10.138 10.227.10.141)
+port=8080
+
+set_whitelist_one() {
+	ip=$1
+	whitelist=$2
+    echo "set whitelist for " $ip
+    curl "http://$ip:$port/HiveService/monitor.htm?action=setWhitelist&whitelist="$whitelist
+    echo "\n"
+}
 
 set_whitelist() {
 	whitelist=$1
 	for ip in $server_ips
 	do
-		echo "set whitelist for " $ip
-        curl "http://$ip:8080/HiveService/monitor.htm?action=setWhitelist&whitelist="$whitelist
-		echo "\n"
+		set_whitelist_one $ip $whitelist
 	done
+}
+
+get_whitelist_one() {
+	ip=$1
+	echo "whitelist of " $ip ":"
+    curl "http://$ip:$port/HiveService/monitor.htm?action=getWhitelist"
+    echo "\n"
+
 }
 
 get_whitelist() {
 	for ip in $server_ips
 	do
-		echo "whitelist of " $ip ":"
-        curl "http://$ip:8080/HiveService/monitor.htm?action=getWhitelist"
-		echo "\n"
+		get_whitelist_one $ip
 	done
+}
+
+set_blacklist_one() {
+	ip=$1
+	blacklist=$2
+	echo "set blacklist for " $ip
+    curl "http://$ip:$port/HiveService/monitor.htm?action=setBlacklist&blacklist="$blacklist
+    echo "\n"
+	
 }
 
 set_blacklist() {
 	blacklist=$1
 	for ip in $server_ips
 	do
-		echo "set blacklist for " $ip
-        curl "http://$ip:8080/HiveService/monitor.htm?action=setBlacklist&blacklist="$blacklist
-		echo "\n"
+		set_blacklist_one $ip $blacklist
 	done
+}
+
+get_blacklist_one() {
+	ip=$1
+	echo "blacklist of " $ip ":"
+    curl "http://$ip:$port/HiveService/monitor.htm?action=getBlacklist"
+	echo "\n"
+
 }
 
 get_blacklist() {
 	for ip in $server_ips
 	do
-		echo "blacklist of " $ip ":"
-        curl "http://$ip:8080/HiveService/monitor.htm?action=getBlacklist"
-		echo "\n"
+		get_blacklist_one $ip
 	done
 }
 
+set_threshold_one() {
+	ip=$1
+	threshold=$2
+	echo "set threshold for " $ip
+    curl "http://$ip:$port/HiveService/monitor.htm?action=setThreshold&t="$threshold
+	echo "\n"
+	
+}
 
 set_threshold() {
 	threshold=$1
 	for ip in $server_ips
 	do
-		echo "set threshold for " $ip
-        curl "http://$ip:8080/HiveService/monitor.htm?action=setThreshold&t="$threshold
-		echo "\n"
+		set_threshold_one $ip $threshold
 	done
+}
+
+get_threshold_one() {
+	ip=$1
+    echo "threshold of " $ip ":"
+    curl "http://$ip:$port/HiveService/monitor.htm?action=getThreshold"
+	echo "\n"
+
 }
 
 get_threshold() {
 	for ip in $server_ips
 	do
-		echo "threshold of " $ip ":"
-        curl "http://$ip:8080/HiveService/monitor.htm?action=getThreshold"
-		echo "\n"
+		get_threshold_one $ip
 	done
 }
 
@@ -64,7 +102,7 @@ get_msg_cnt() {
 	for ip in $server_ips
 	do
 		echo "message count of " $ip ":"
-        curl "http://$ip:8080/HiveService/monitor.htm?action=getCnt"
+        curl "http://$ip:$port/HiveService/monitor.htm?action=getCnt"
 		echo "\n"
 	done
 }
