fd963bb0b8e22b8215bf32a53373062d63ade7aa
James Xu <xumingming64398966@gmail.com>
2013-11-18 13:36:49 +0800

    add libdir

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-ritz "0.7.0"]
                   [no-man-is-an-island/lein-eclipse "2.0.0"]
-                  [jonase/eastwood "0.0.2"]]
+                  [jonase/eastwood "0.0.2"]
+                  [lein-libdir "0.1.1"]]
         :dependencies [
                        [ritz/ritz-nrepl-middleware "0.7.0"]]
         :repl-options { ;;:init (use 'tracer.core)
