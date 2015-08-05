6972ed0220e6837cd83040e2ff5bafa1588f8a69
James Xu <xumingmingv@gmail.com>
2015-08-05 15:56:18 +0800

    cleanup

--- .emacs.d/elpa/ergoemacs-mode-20141021.756/ergoemacs-mode-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "ergoemacs-mode" "20141021.756" "Emacs mode based on common modern interface and ergonomics."
-  '((emacs "24.1")
-    (undo-tree "0.6.5"))
-  :url "https://github.com/ergoemacs/ergoemacs-mode" :keywords
-  '("convenience"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
