d8d052e2c1af6e1f0765a3af11649af1286bbbde
James Xu <xumingmingv@gmail.com>
2016-04-30 00:56:31 +0800

    cleanup

--- sanity_check.sh
+++ sanity_check.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-egrep 'alipay|alibaba' -r . | grep -v auto-save-list | grep -v projectile-bookmarks.eld | grep -v recentf | grep -v projectile.cache | grep -v sanity_check.sh
+egrep 'alipay|alibaba' -ri . | grep -v auto-save-list | grep -v projectile-bookmarks.eld | grep -v recentf | grep -v projectile.cache | grep -v sanity_check.sh
